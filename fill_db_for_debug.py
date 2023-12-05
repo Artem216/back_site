a8a9a8216e71d0b01fba6d4dc21ff84aece49f57
Necros <russarr@gmail.com>
2023-12-05 19:54:40 +0500

    feat: add script to fill db with instrumets for debug

---
+++ fill_db_for_debug.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from src.db.models import Instrument
+from config.settings import settings
+
+from sqlalchemy import URL, create_engine
+
+
+engine_url = URL.create(
+    "postgresql+psycopg2",
+    username=settings.postgres_user,
+    password=settings.postgres_password,
+    host=settings.postgres_host,
+    port=settings.postgres_port_number,
+    database=settings.postgres_db,
+)
+
+engine = create_engine(engine_url)
+
+instruments = (
+        Instrument(code="AIG-RM", title="American International ORD SHS", group="stock_shares"),
+        Instrument(code="AMEZ", title="Ашинский метзавод ПАО ао", group="stock_shares"),
+        Instrument(code="MAGEP", title="&quot;Магаданэнерго&quot; ПАО ап", group="stock_shares"),
+        )
+
+with Session(engine) as session:
+    session.add_all(instruments)
+    session.commit()
+
+
+
+
+
+
