26ce58449b1d3948fc72cac9f0fbfa7cd1a64ae5
Necros <russarr@gmail.com>
2023-12-07 16:36:06 +0500

    fix: changes in data access architecture

--- src/trader/repository.py
+++ src/trader/repository.py
@@ -1,36 +1,35 @@
-from typing import Sequence
 import uuid
-from sqlalchemy import select
-from src.db.models import Instrument, User
+from typing import Sequence
+
+from sqlalchemy import and_, select
+from src.db import schemas
+from src.db.models import Deal, DealType, Instrument, User
 from src.db.repository import AbstractRepository
 from src.db.sql import SQLManager
 from src.utils.logger import conf_logger as logger
-from src.db import schemas
 
 
-class InstrumentRepository(AbstractRepository):
+class InstrumentDAL:
+    """Data access layer for instruments"""
+
     instance = None
 
     def __init__(self, db_manager: SQLManager) -> None:
         super().__init__()
         self.db = db_manager
-        self.logger = logger("InsturmentRepository")
+        self.logger = logger("InsturmentDAL", "D")
 
     def __new__(cls, *args, **kwargs):
         """Singleton pattern"""
         if cls.instance is None:
-            cls.instance = super(InstrumentRepository, cls).__new__(cls)
+            cls.instance = super(InstrumentDAL, cls).__new__(cls)
         return cls.instance
 
     def add(
         self,
         instrument_data: schemas.InstrumentCreate,
     ) -> Instrument:
-        # instrument = Instrument(**instrument_data.model_dump())
-        instrument = Instrument(
-                code= instrument_data.code,
-                title= instrument_data.title
-                )
+        instrument = Instrument(code=instrument_data.code, title=instrument_data.title)
         self.db.session.add(instrument)
         self.db.session.commit()
 
@@ -52,7 +51,9 @@ class InstrumentRepository(AbstractRepository):
     def get_all(self) -> list[Instrument]:
         return list(self.db.session.scalars(select(Instrument)).all())
 
-    def add_user_instrument(self, user_id: uuid.UUID, instrument_data: schemas.InstrumentBase) -> schemas.Instrument:
+    def add_user_instrument(
+        self, user_id: uuid.UUID, instrument_data: schemas.InstrumentBase
+    ) -> schemas.Instrument:
         instrument = self.get(instrument_data)
         if not instrument:
             instrument = Instrument(**instrument_data.model_dump())
@@ -68,44 +69,67 @@ class InstrumentRepository(AbstractRepository):
         return self.db.session.scalars(stmt).one().instruments
 
 
-class DealRepository(AbstractRepository):
+class DealDAL:
+    """Data access layer for deals"""
+
     instance = None
 
     def __init__(self, db_manager: SQLManager) -> None:
         super().__init__()
         self.db = db_manager
-        self.logger = logger("DealRepository")
+        self.logger = logger("DealRepository", "D")
 
     def __new__(cls, *args, **kwargs):
         """Singleton pattern"""
         if cls.instance is None:
-            cls.instance = super(DealRepository, cls).__new__(cls)
+            cls.instance = super(DealDAL, cls).__new__(cls)
         return cls.instance
 
     def add(
         self,
-        instrument_data: schemas.InstrumentCreate,
-    ) -> Instrument:
-        instrument = Instrument(**instrument_data.model_dump())
-        self.db.session.add(instrument)
+        deal_data: schemas.DealBase,
+    ) -> Deal:
+        deal = Instrument(**deal_data.model_dump())
+        self.db.session.add(deal)
         self.db.session.commit()
 
-        return instrument
+        return deal
 
-    def get(self, code: str) -> Instrument | None:
-        return self.db.session.get(Instrument, code)
+    def get_user_deals(self, user_id: str) -> list[Deal] | None:
+        user = self.db.session.get(User, user_id)
+        if user:
+            return user.deals
 
-    def update(self, user: User):
-        self.db.session.add(user)
-        self.db.session.commit()
+    def get_user_deals_by_instrument(
+        self, user_id: str, deals_request: schemas.UserDealsRequest
+    ):  # -> list[schemas.Deal]:
+        self.logger.debug(f"{deals_request=}")
+        stmt = select(Deal).where(
+            and_(
+                Deal.instrument_code == deals_request.instrument_code,
+                Deal.user_id == user_id,
+            )
+        )
+        deals = list(self.db.session.scalars(stmt).all())
+        return [schemas.Deal(
+            id=deal.id,
+            price=deal.price,
+            quantity=deal.quantity,
+            deal_type=deal.deal_type,
+            user=deal.user.id,
+            instrument=deal.instrument_code,
+            datetime=deal.date_time,
+        ) for deal in deals]
 
-    def delete(self, code: str):
-        instrument = self.db.session.get(Instrument, code)
-        if instrument:
-            self.db.session.delete(instrument)
-            self.db.session.commit()
+        # return [schemas.DealBase.model_validate(deal) for deal in deals]
+        # return [schemas.Deal(
+        #     id=deal.id,
+        #     price=deal.price,
+        #     quantity=deal.quantity,
+        #     deal_type=deal.deal_type,
+        #     user=deal.user.id,
+        #     instrument=deal.instrument,
+        #     datetime=deal.datetime
+        #     ) for deal in deals]
 
-    # def get_user_deals(self, user_id: uuid.UUID) -> list[DealSchema]:
-    #     stmt = select(User).where(User.id == user_id)
-    #     deals = self.db.session.scalars(stmt).one().deals
-    #     return [DealSchema.model_validate(deal) for deal in deals]
+
