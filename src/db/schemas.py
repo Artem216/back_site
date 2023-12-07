163518a6c04651e729229f2857e9a0ae20cdf123
Necros <russarr@gmail.com>
2023-12-07 21:25:30 +0500

    fix: optimize schemas

--- src/db/schemas.py
+++ src/db/schemas.py
@@ -6,30 +6,19 @@ from datetime import datetime
 from decimal import Decimal
 
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
 from src.db.models import DealType
 
 
-# class DealType(enum.Enum):
-#     model_config = ConfigDict(from_attributes=True)
-#
-#     buy = "buy"
-#     sell = "sell"
-
-
 class InstrumentBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     code: str = Field(..., min_length=1, max_length=50)
 
 
-class InstrumentCreate(InstrumentBase):
+class Instrument(InstrumentBase):
     title: str = Field(..., min_length=1, max_length=256)
     group: str = Field(..., min_length=1, max_length=50)
-
-
-class Instrument(InstrumentBase):
-    model_config = ConfigDict(from_attributes=True)
-
-    deals: list[Deal] = []
-    users: list[User] = []
 
 
 class DealBase(BaseModel):
@@ -50,14 +39,6 @@ class UserDealsRequest(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     instrument_code: str
 
-# class UserDealResponse(BaseModel):
-    # price: Decimal = Field(ge=0.01, decimal_places=2)
-    # quantity: int = Field(ge=1)
-    # deal_type: DealType
-    # user: str
-    # instrument: Instrument
-    # id: int
-    # datetime: datetime
 
 class UserBase(BaseModel):
     email: EmailStr
