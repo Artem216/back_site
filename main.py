193343346cdffd9715b450ae4ca4efe1a0f2c6b6
Artem216 <artemtsykanov22@gmail.com>
2023-12-07 22:22:50 +0300

    small changes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import uvicorn
 from src.app import create_app
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = create_app()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Set this to the appropriate origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Set this to the allowed HTTP methods
+    allow_headers=["*"],  # Set this to the allowed headers
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, port=9999, host="0.0.0.0")
