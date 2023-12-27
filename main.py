e0a846c5207b8798bbda86ef8db995a70d31ea7a
Artem216 <artemtsykanov22@gmail.com>
2023-12-27 11:56:46 +0300

    final final

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 import uvicorn
-from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 
-from app import create_app
+from src.app import create_app
 
 
 app = create_app()
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
-)
 
 
 
--- src/app.py
+++ src/app.py
@@ -2,12 +2,24 @@ from fastapi import FastAPI
 from src.api import api_router
 from src.auth.endpoints import router as auth_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 def create_app():
     _app = FastAPI(
         name="StroyVigoda",
         description="API",
     )
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  
+        allow_credentials=True,
+        allow_methods=["*"],  
+        allow_headers=["*"],  
+    )
+
+
     _app.include_router(api_router)
     _app.include_router(auth_router)
     return _app
